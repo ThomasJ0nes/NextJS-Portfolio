b012a5eb12ce5c15c9098ea73422d347831cb116
Tom Jones <94535525+ThomasJ0nes@users.noreply.github.com>
2023-12-30 13:46:14 +0000

    Backend Form Functionality

---
+++ app/api/createtable/route.ts
@@ -0,0 +1,18 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  try {
+    const result = await sql`
+      ALTER TABLE contact (
+        email varchar(255),
+        subject varchar(255),
+        message varchar(255),
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
