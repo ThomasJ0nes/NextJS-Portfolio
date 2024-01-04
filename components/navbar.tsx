88f1deb4504fc2f3f312299feffd50b7ec321e59
Tom Jones <94535525+ThomasJ0nes@users.noreply.github.com>
2024-01-04 17:10:18 +0000

    HoverCards

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -57,7 +57,7 @@ const Navbar = () => {
           <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
             <li>
               <a
-                href="#"
+                href="https://github.com/ThomasJ0nes"
                 className="block py-2 mt-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Github
@@ -65,7 +65,7 @@ const Navbar = () => {
             </li>
             <li>
               <a
-                href="#"
+                href="https://twitter.com/TomyJxyz"
                 className="block py-2 mt-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Twitter
@@ -73,7 +73,7 @@ const Navbar = () => {
             </li>
             <li>
               <a
-                href="#"
+                href="https://www.linkedin.com/in/thomas-jones-106920245/"
                 className="block mt-2 py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 LinkedIn
--- components/techStack.tsx
+++ components/techStack.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 const TechStack = () => {
   return (
     <section>
@@ -8,6 +13,7 @@ const TechStack = () => {
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold">
             My Technology Stack...
           </h2>
+
           <p className="text-gray-500 sm:text-xl dark:text-gray-400">
             Below is the list of my strongest skills and technologies I use to
             build my projects. However i am self taught and this is only my
@@ -20,7 +26,29 @@ const TechStack = () => {
             <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
               <img src="nextjs.svg"></img>
             </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">NextJS</h3>
+            <HoverCard>
+              <HoverCardTrigger>
+                <h3 className="mb-2 underline text-xl font-bold dark:text-white">
+                  NextJS
+                </h3>
+              </HoverCardTrigger>
+              <HoverCardContent>
+                <div className="flex justify-between space-x-4 w-full">
+                  <Avatar>
+                    <AvatarImage src="https://github.com/vercel.png" />
+                    <AvatarFallback>VC</AvatarFallback>
+                  </Avatar>
+                  <div className="space-y-2">
+                    <h4 className="text-sm font-semibold">@nextjs</h4>
+                    <p className="text-sm">
+                      Created by vercel NextJS is a React framework for building
+                      fast and scalable web applications. It is a cutting edge
+                      framework and intergrates well with other technologies.
+                    </p>
+                  </div>
+                </div>
+              </HoverCardContent>
+            </HoverCard>
             <p className="text-gray-500 dark:text-gray-400">
               I build most of my apps in NextJS as it is a great framework for
               building fast, scalable and SEO friendly web apps. Its a cutting
@@ -31,9 +59,32 @@ const TechStack = () => {
             <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
               <img src="javascript.svg"></img>
             </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">
-              Javascript
-            </h3>
+            <HoverCard>
+              <HoverCardTrigger>
+                <h3 className="mb-2 underline text-xl font-bold dark:text-white">
+                  Javascript
+                </h3>
+              </HoverCardTrigger>
+
+              <HoverCardContent>
+                <div className="flex justify-between space-x-4 w-full">
+                  <Avatar>
+                    <AvatarImage src="javascript.svg" />
+                    <AvatarFallback>VC</AvatarFallback>
+                  </Avatar>
+                  <div className="space-y-2">
+                    <h4 className="text-sm font-semibold">@Javascript</h4>
+                    <p className="text-sm">
+                      Created by Brendan Eich in 1995, Javascript is a powerful
+                      programming language that is used to create interactive
+                      effects within web browsers. It is the most popular
+                      programming language in the world and is used by 95% of
+                      all websites.
+                    </p>
+                  </div>
+                </div>
+              </HoverCardContent>
+            </HoverCard>
             <p className="text-gray-500 dark:text-gray-400">
               I use javascript for many my web apps, I am very comfortable with
               the language and have been using it for well over 3 years now.
@@ -44,9 +95,32 @@ const TechStack = () => {
             <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
               <img src="typescript.svg"></img>
             </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">
-              TypeScript
-            </h3>
+            <HoverCard>
+              <HoverCardTrigger>
+                <h3 className="mb-2 underline text-xl font-bold dark:text-white">
+                  TypeScript
+                </h3>
+              </HoverCardTrigger>
+
+              <HoverCardContent>
+                <div className="flex justify-between space-x-4 w-full">
+                  <Avatar>
+                    <AvatarImage src="typescript.svg" />
+                    <AvatarFallback>VC</AvatarFallback>
+                  </Avatar>
+                  <div className="space-y-2">
+                    <h4 className="text-sm font-semibold">@Typescript</h4>
+                    <p className="text-sm">
+                      Created by Anders Hejlsberg at Microsoft in 2010
+                      Typescript is a superset of Javascript that adds static
+                      typing to the language. It is a powerful language that is
+                      used to create large scale applications. It is a very fast
+                      and reliable language and its popularity is growing.
+                    </p>
+                  </div>
+                </div>
+              </HoverCardContent>
+            </HoverCard>
             <p className="text-gray-500 dark:text-gray-400">
               I have been using typescript for a few months now and I am
               starting to get comfortable with it. I am still learning the
@@ -57,9 +131,31 @@ const TechStack = () => {
             <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
               <img src="tailwindcss.svg"></img>
             </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">
-              Tailwind CSS
-            </h3>
+            <HoverCard>
+              <HoverCardTrigger>
+                <h3 className="mb-2 underline text-xl font-bold dark:text-white">
+                  Tailwind CSS
+                </h3>
+              </HoverCardTrigger>
+              <HoverCardContent>
+                <div className="flex justify-between space-x-4 w-full">
+                  <Avatar>
+                    <AvatarImage src="tailwindcss.svg" />
+                    <AvatarFallback>VC</AvatarFallback>
+                  </Avatar>
+                  <div className="space-y-2">
+                    <h4 className="text-sm font-semibold">@Tailwind</h4>
+                    <p className="text-sm">
+                      Created by Adam Wathan, Jonathan Reinink, David Hemphill
+                      and Steve Schoger in 2017, Tailwind CSS is a utility-first
+                      CSS framework for rapidly building custom user interfaces.
+                      It is a very powerful framework that is used to create
+                      beautiful modern designs.
+                    </p>
+                  </div>
+                </div>
+              </HoverCardContent>
+            </HoverCard>
             <p className="text-gray-500 dark:text-gray-400">
               Tailwind CSS is my go to CSS framework, I love the utility first
               approach and the ability to customise the framework to my liking.
@@ -71,7 +167,33 @@ const TechStack = () => {
             <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
               <img className="h-12" src="firebase.svg"></img>
             </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">Firebase</h3>
+            <HoverCard>
+              <HoverCardTrigger>
+                <h3 className="mb-2 underline text-xl font-bold dark:text-white">
+                  Firebase
+                </h3>
+              </HoverCardTrigger>
+
+              <HoverCardContent>
+                <div className="flex justify-between space-x-4 w-full">
+                  <Avatar>
+                    <AvatarImage src="firebase.svg" />
+                    <AvatarFallback>VC</AvatarFallback>
+                  </Avatar>
+                  <div className="space-y-2">
+                    <h4 className="text-sm font-semibold">@Firebase</h4>
+                    <p className="text-sm">
+                      Created by Google in 2011, Firebase is a Backend as a
+                      Service (BaaS) that provides developers with a variety of
+                      tools and services to help them develop quality apps, grow
+                      their user base, and earn profit. Firebase is built on
+                      Google’s infrastructure and scales automatically, for even
+                      the largest apps.
+                    </p>
+                  </div>
+                </div>
+              </HoverCardContent>
+            </HoverCard>
             <p className="text-gray-500 dark:text-gray-400">
               Firebase is my go to backend for most of my projects, Its been a
               game changer for me in its speed reliability and intergration with
@@ -83,7 +205,35 @@ const TechStack = () => {
             <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
               <img className="h-16" src="eth.svg"></img>
             </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">Solidity</h3>
+
+            <HoverCard>
+              <HoverCardTrigger>
+                <h3 className="mb-2 underline text-xl font-bold dark:text-white">
+                  Solidity
+                </h3>
+              </HoverCardTrigger>
+
+              <HoverCardContent>
+                <div className="flex justify-between space-x-4 w-full">
+                  <Avatar>
+                    <AvatarImage src="eth.svg" />
+                    <AvatarFallback>VC</AvatarFallback>
+                  </Avatar>
+                  <div className="space-y-2">
+                    <h4 className="text-sm font-semibold">@Solidity</h4>
+                    <p className="text-sm">
+                      Created by Gavin Wood, Solidity is an object-oriented
+                      programming language for writing smart contracts. It is
+                      used for implementing smart contracts on various
+                      blockchain platforms, most notably, Ethereum. It is a
+                      statically typed language that supports inheritance,
+                      libraries and complex user-defined types among other
+                      features.
+                    </p>
+                  </div>
+                </div>
+              </HoverCardContent>
+            </HoverCard>
             <p className="text-gray-500 dark:text-gray-400">
               Solidity is a new endeavour for me, as the web3 space is growing I
               am learning solidity to build smart contracts and decentralised
