f8fd322b976a78d3a141efa37e2dbe463ddafd44
Tom Jones <94535525+ThomasJ0nes@users.noreply.github.com>
2024-03-18 14:40:19 +0000

    Remove Fade on Hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import TextScramble from "./textScramble";
 
 import { useEffect, useRef } from "react";
-import { FadeIn } from "./FadeIn";
+
 const Hero = () => {
   const textRef = useRef<HTMLDivElement>(null);
   let fx: TextScramble;
@@ -36,30 +36,27 @@ const Hero = () => {
 
   return (
     <>
-      <FadeIn>
-        <section className="">
-          <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-            <div className="mr-auto place-self-center lg:col-span-7">
-              <h1
-                ref={textRef}
-                className="max-w-2xl mb-4 text-3xl sm:text-2xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white"
-              ></h1>
-              <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-lg dark:text-gray-400">
-                I am a Full Stack Front End Developer with a passion for
-                creating beautiful, responsive and accessible websites and
-                applications.
-              </p>
-            </div>
-            <div className=" lg:block lg:mt-0 lg:col-span-5">
-              <img
-                className="rounded"
-                src="https://cdn.dribbble.com/users/1818304/screenshots/6601989/rick__dribbble.gif"
-                alt="mockup"
-              />
-            </div>
+      <section className="">
+        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div className="mr-auto place-self-center lg:col-span-7">
+            <h1
+              ref={textRef}
+              className="max-w-2xl mb-4 text-3xl sm:text-2xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white"
+            ></h1>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-lg dark:text-gray-400">
+              I am a Full Stack Front End Developer with a passion for creating
+              beautiful, responsive and accessible websites and applications.
+            </p>
           </div>
-        </section>
-      </FadeIn>
+          <div className=" lg:block lg:mt-0 lg:col-span-5">
+            <img
+              className="rounded"
+              src="https://cdn.dribbble.com/users/1818304/screenshots/6601989/rick__dribbble.gif"
+              alt="mockup"
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
